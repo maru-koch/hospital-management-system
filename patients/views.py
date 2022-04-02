49025a42033ba5128b4eef09a9af71aa56861e6a
Rilly_ <chile.simon@4e4email.net>
2022-04-03 00:06:14 +0100

    added .gitignore file and url paths

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Patient,Practitioner,User
+
+
+def index(request):
+    return render(request,'patients/index.html')
+    
+
+class PatientRecordsView(generic.DetailView):
+    pass
+
+class BookAppointmentView(generic.CreateView):
+    pass
 
 # Create your views here.
