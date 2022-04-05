fb7ae52d291ff0f53e315c12466c198399f843e6
akorede olawole <akoredeolawole@gmail.com>
2022-04-05 21:53:31 +0100

    Delete patients directory

--- patients/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User,Patient,Practitioner
-
-
-admin.site.register(User)
-admin.site.register(Patient)
-admin.site.register(Practitioner)
-
-# Register your models here.
