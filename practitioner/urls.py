600c5cc04c7264365e2d0c83be8bdca66f3e0c17
Akorede olawole <akoredeolawole@gmail.com>
2022-04-05 23:55:43 +0100

    implemented a new practitioner app

---
+++ practitioner/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from practitioner import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+#---------FOR DOCTOR RELATED URLS-------------------------------------
+urlpatterns =[
+    path('doctor-dashboard', views.doctor_dashboard_view,name='doctor-dashboard'),
+
+    path('doctor-patient', views.doctor_patient_view,name='doctor-patient'),
+    path('doctor-view-patient', views.doctor_view_patient_view,name='doctor-view-patient'),
+    path('doctor-view-discharge-patient',views.doctor_view_discharge_patient_view,name='doctor-view-discharge-patient'),
+
+    path('doctor-appointment', views.doctor_appointment_view,name='doctor-appointment'),
+    path('doctor-view-appointment', views.doctor_view_appointment_view,name='doctor-view-appointment'),
+    path('doctor-delete-appointment',views.doctor_delete_appointment_view,name='doctor-delete-appointment'),
+    path('delete-appointment/<int:pk>', views.delete_appointment_view,name='delete-appointment'),
+]
