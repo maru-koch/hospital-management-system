73df510e9b67c4400ce763e4e177138c06b74b5d
maru-koch <nwokochafranklyn@gmail.com>
2022-04-06 02:32:14 +0100

    connected the patient view page to the register and the login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('patientsignup', views.patient_signup_view),
+    path('patientsignup', views.patient_signup_view, name = 'patientsignup'),
     path('adminlogin', LoginView.as_view(template_name='accounts/adminlogin.html'), name='adminlogin'),
     path('doctorlogin', LoginView.as_view(template_name='accounts/doctorlogin.html'), name='doctorlogin'),
     path('patientlogin', LoginView.as_view(template_name='accounts/patientlogin.html'), name='patientlogin'),
